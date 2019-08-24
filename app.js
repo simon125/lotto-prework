d2110cd34f42bca05848e8d685cc79d64a5aa5f7
Szymon Oleszek <szymqwerty@gmail.com>
2019-08-25 01:28:54 +0200

    Basic version ended
    - added functions with order to inctructions
    - added require html and css
    - added animations from bounty lib
    - todo cashing previos values and seting them for initial values during reloading

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * 49) + 1;
+};
 const setup = () => {
-  console.log(123);
+  const numbers = buildArrayWithRandomNumbers().sort((a, b) => a - b);
+
+  render(numbers);
+  document.getElementById("randomBtn").addEventListener("click", setup);
+};
+const buildArrayWithRandomNumbers = () => {
+  const randomNumbers = [];
+  while (randomNumbers.length < 6) {
+    const randomNumber = getRandomNumber();
+    if (!randomNumbers.includes(randomNumber)) {
+      randomNumbers.push(randomNumber);
+    }
+  }
+  return randomNumbers;
+};
+
+const render = numbers => {
+  const ballContainer = document.getElementById("ballContainer");
+  ballContainer.innerHTML = "";
+  const template = number => {
+    return `<span class="shadow"><div class="ball ball-${number}"></div></span>`;
+  };
+
+  const balls = numbers.map(template).join("");
+  ballContainer.innerHTML = balls;
+  numbers.forEach(number => {
+    window.bounty.default({
+      el: `.ball-${number}`,
+      value: number,
+      animationDelay: 100,
+      letterAnimationDelay: 100
+    });
+  });
 };
 
 document.addEventListener("DOMContentLoaded", setup);
-
-/*
-Opis zadania
-Zadanie polega na tym, aby:
-
-Wypełnić tablicę 6-cioma losowymi liczbami od 1 do 49
-Zaprezentować na stronie internetowej
-Liczby powinny być posortowane alfanumerycznie rosnąco
-Krok po kroku footprints
-Stwórz katalog lotto.
-Wewnątrz katalogu stwórz pliki index.html oraz scripts/main.js.
-W pliku scripts/main.js zrób funkcję setup
-Uruchom funkcję setup po załadowaniu DOMa (zdarzenie DOMContentLoaded)
-Stwórz funkcję buildArrayWithRandomNumbers
-Przypisz wynik funkcji buildArrayWithRandomNumbers do zmiennej numbers
-Stwórz funkcję render
-Uruchom funkcję render w funkcji setup przekazując jej tablicę z liczbami
-Stwórz funkcję getRandomNumber, która będzie zwracać losową liczbę
-Stwórz funkcję template, która będzie zwracać strukturę HTML dla pojedynczej liczby - będzie to funkcja zdefiniowana wewnątrz funkcji render
-Dołącz plik CSS i zamień dodaj wygląd kulki do każdego elementu zawierającego losową liczbę
-Zadanie dodatkowe
-Wybieranie kolejnych liczb powinno być zaprezentowane w formie animacji z użyciem https://github.com/coderitual/bounty
-Dodaj w CSS cień wewnętrzny, aby uzyskać efekt przestrzeni*/
